6bafe6dcf18be29521e0c5f9206719ff8c28d649
Nikhil <nikhil@example.com>
2020-02-28 15:16:01 +0530

    gardleExercise

--- ttn-bootcamp-junit-master/src/test/java/com/im/FirstTest.java
+++ ttn-bootcamp-junit-master/src/test/java/com/im/FirstTest.java
@@ -1,6 +1,7 @@
 package com.im;
 
 //import org.junit.Test;
+
 import junit.runner.Version;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -17,15 +18,15 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FirstTest {
-    @Test
-    void canary(){
+
+    /*void canary() {
         assertTrue(true);
         System.out.println("Test Executed......");
 
-    }
+    }*/
 
     @Test
-    void should_check_for_average(){
+    void should_check_for_average() {
         List<BigDecimal> list = new ArrayList<BigDecimal>();
         list.add(new BigDecimal(10.333));
         list.add(new BigDecimal(10.333));
@@ -35,23 +36,23 @@ public class FirstTest {
 
         BigDecimal result = new First().calculateAverage(list);
         BigDecimal expected = new BigDecimal(10.333);
-        assertEquals(result,expected);
+        assertEquals(result, expected);
     }
 
     @Test
-    void should_check_for_null_exception(){
+    void should_check_for_null_exception() {
         List<BigDecimal> list = new ArrayList<BigDecimal>();
-        Executable executable = ()-> new First().calculateAverage(list);
+        Executable executable = () -> new First().calculateAverage(list);
 
-        assertThrows(RuntimeException.class,executable);
+        assertThrows(RuntimeException.class, executable);
 
     }
 
     //@ParameterizedTest
     //@ValueSource(String ={"Nitin","Nikhil","Mahak"})
     @ParameterizedTest
-    @CsvSource(value={"Nikhil","Nitin","Nikina"})
-    void check_for_palindrome(String  val){
+    @CsvSource(value = {"Nikhil", "Nitin", "Nikina"})
+    void check_for_palindrome(String val) {
         String str = val;
         boolean actual = new First().isPallindrome(str);
         assertFalse(actual);
@@ -59,28 +60,38 @@ public class FirstTest {
     }
 
     @Test
-    void check_for_even_element(){
+    void check_for_even_element() {
         List<Integer> ls = new ArrayList<Integer>();
         ls.add(234);
         ls.add(239);
         ls.add(238);
         ls.add(237);
         ls.add(236);
-        List<Integer> expected = Arrays.asList(239,237);
+        List<Integer> expected = Arrays.asList(239, 237);
         List<Integer> actual = new First().filterEvenElements(ls);
-        Integer [] exp = expected.stream().toArray(Integer []::new);
-        Integer [] act = actual.stream().toArray(Integer[]::new);
+        Integer[] exp = expected.stream().toArray(Integer[]::new);
+        Integer[] act = actual.stream().toArray(Integer[]::new);
         //for(Integer e:actual)
-          //  System.out.println("values:"+e);
-        assertArrayEquals(exp,act);
+        //  System.out.println("values:"+e);
+        assertArrayEquals(exp, act);
 
     }
 
     @Test
-    void check_for_null_String(){
+    void check_for_null_String() {
         String str = "";
         boolean expected = true;
         boolean actual = new First().isPallindrome(str);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void check_for_Replace_substring(){
+        String main = "Nikhl";
+        String sub = "i";
+        String tar = "ii";
+        String expected = "Niikhl";
+        String actual = new First().replaceSubString(main,sub,tar);
         assertEquals(expected,actual);
     }
 
